fb984f058d036ce54794db58bd795e9e59837260
paresh-2002 <pareshparesh.ay99099@gmail.com>
2025-03-24 16:31:01 +0530

    add media query

--- index.js
+++ index.js
@@ -116,3 +116,18 @@ rentalDeals.map((item) => (
     `
 ))
 
+document.querySelector(".navbar-toggler-btn").addEventListener("click", function () {
+    const navMenu = document.getElementById("navMenu");
+    navMenu.classList.toggle("active");
+
+    // Toggle hamburger icon between bars and X
+    const hamburgerIcon = this.querySelector("i");
+    if (hamburgerIcon.classList.contains("fa-bars")) {
+      hamburgerIcon.classList.remove("fa-bars");
+      hamburgerIcon.classList.add("fa-xmark");
+    } else {
+      hamburgerIcon.classList.remove("fa-xmark");
+      hamburgerIcon.classList.add("fa-bars");
+    }
+  });
+
